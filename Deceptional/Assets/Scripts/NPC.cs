5dd2799ba2a871fe92637488a43101c5ab1407d3
Oscar Losada <oscarlosu@gmail.com>
2016-03-12 23:06:56 +0100

    Implemented NPCPart. Started working on NPC: GoToInterrogation, Assemble and basic setup for animations.

---
+++ Deceptional/Assets/Scripts/NPC.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPC : MonoBehaviour
+{
+
+    public string Name;
+    public Conversation Conversation { get; set; }
+
+    public bool Mood { get; set; }
+    //public bool Mood;
+
+    public bool IsKiller { get; set; }
+    //public bool IsKiller    
+
+    public SkinnedMeshRenderer HeadRenderer;
+    public SkinnedMeshRenderer TorsoRenderer;
+    public SkinnedMeshRenderer LegsRenderer;
+
+    private NPCPart head;
+    private NPCPart torso;
+    private NPCPart legs;
+
+    private NavMeshAgent navAgent;
+    public Vector3 InterrogationPosition;
+    private Animator anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        // TESTS
+        // Assemble test
+        NPCPart head = new NPCPart(NPCPart.NPCPartType.Hat, NPCPart.NPCPartDescription.Black);
+        NPCPart torso = new NPCPart(NPCPart.NPCPartType.Shirt, NPCPart.NPCPartDescription.Yellow);
+        NPCPart legs = new NPCPart(NPCPart.NPCPartType.Pants, NPCPart.NPCPartDescription.Red);
+        Assemble(head, torso, legs);
+        // Test navmesh agent
+        GoToInterrogation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Assemble(NPCPart head, NPCPart torso, NPCPart legs)
+    {
+        // Save parts
+        this.head = head;
+        this.torso = torso;
+        this.legs = legs;
+        // Load meshes and materials
+        // Head
+        HeadRenderer.sharedMesh = Resources.Load<Mesh>("Models/" + this.head.Type.ToString());
+        HeadRenderer.sharedMaterial = Resources.Load<Material>("Materials/" + this.head.Description.ToString());
+        // Torso
+        TorsoRenderer.sharedMesh = Resources.Load<Mesh>("Models/" + this.torso.Type.ToString());
+        TorsoRenderer.sharedMaterial = Resources.Load<Material>("Materials/" + this.torso.Description.ToString());
+        // Legs
+        LegsRenderer.sharedMesh = Resources.Load<Mesh>("Models/" + this.legs.Type.ToString());
+        LegsRenderer.sharedMaterial = Resources.Load<Material>("Materials/" + this.legs.Description.ToString());
+    }
+
+    public void GoToInterrogation()
+    {
+        navAgent.SetDestination(InterrogationPosition);
+        StartCoroutine(HandleWalkAnimation());
+    }
+
+    private IEnumerator HandleWalkAnimation()
+    {
+        Debug.Log(navAgent.remainingDistance);
+        // Start walk animation        
+        anim.SetBool("Walk", true);
+
+        // Wait for agent to reach destination
+        do
+        {
+            yield return null;
+        } while (!Mathf.Approximately(navAgent.remainingDistance, 0.0f));
+        // Stop walk animation
+        anim.SetBool("Walk", false);
+    }
+
+    public void Behaviour()
+    {
+
+    }
+
+}
---
+++ Deceptional/Assets/Scripts/NPCPart.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NPCPart
+{
+    public enum NPCPartType
+    {
+        Pants,
+        Shirt,
+        Hat
+    }
+    public enum NPCPartDescription
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Black,
+        White
+    }
+
+    public NPCPartType Type { get; set; }
+    public NPCPartDescription Description { get; set; }
+
+    public NPCPart(NPCPartType type, NPCPartDescription description)
+    {
+        Type = type;
+        Description = description;
+    }
+
+    /*
+    private Mesh model;
+    public Mesh Model { get; set; }
+
+    private Material material;
+    public Material Material { get; set; }
+
+    public NPCPart(Mesh model, Material material, NPCPartType type, NPCPartDescription description)
+    {
+        this.model = model;
+        this.material = material;
+        this.type = type;
+        this.description = description;
+    }
+    */
+}
