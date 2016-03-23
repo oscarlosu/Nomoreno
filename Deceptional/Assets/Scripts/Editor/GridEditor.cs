98bfa2a9e4117f0245fcce11b31b91ecbc7aa8b7
Oscar Losada <oscarlosu@gmail.com>
2016-03-23 17:11:53 +0100

    Work on grid.

---
+++ Deceptional/Assets/Scripts/Editor/GridEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Grid))]
+public class GridEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        Grid myScript = (Grid)target;
+        if (GUILayout.Button("Find Adjacent Cells")) {
+            myScript.FindAdjacentCells();
+        }
+    }
+}
--- Deceptional/Assets/Scripts/Grid.cs
+++ Deceptional/Assets/Scripts/Grid.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Grid : MonoBehaviour {
+    private static Grid instance = null;
+    public static Grid Instance {
+        get {
+            if (instance == null) {
+                instance = (Grid)FindObjectOfType(typeof(Grid));
+                if (instance == null)
+                    instance = (new GameObject("Grid")).AddComponent<Grid>();
+            }
+            return instance;
+        }
+    }
     public List<Cell> Cells;
     public List<Cell> FreeCells;
+
+    public float CellSideLength;
+    private float ErrorMargin = 0.5f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,4 +29,31 @@ public class Grid : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
+    public void FindAdjacentCells() {
+        // Find Cells
+        Cell[] cellArray = transform.GetComponentsInChildren<Cell>();
+        List<Cell> cells = new List<Cell>(cellArray);
+        // Sort Cells        
+        List<Cell> sortedCells = cells.OrderBy(c => c.transform.position.x).ToList<Cell>();
+        // Save adjacent cells
+        for(int i = 0; i < sortedCells.Count; ++i) {
+            Cell current = sortedCells[i];
+            current.Adjacent.Clear();
+            for (int j = 0; j < sortedCells.Count; ++j) {
+                Cell other = sortedCells[j];
+                // Skip current cell
+                if (current.GetInstanceID() != other.GetInstanceID()) {
+                    // Stop searching after the x coordinate already guarantees that the rest of the cells are too far
+                    if (Mathf.Abs(current.transform.position.x - other.transform.position.x) < CellSideLength + ErrorMargin) {
+                        if (Mathf.Abs(current.transform.position.z - other.transform.position.z) < CellSideLength + ErrorMargin) {                        
+                            current.Adjacent.Add(other);
+                        }                        
+                    }
+                }
+            }
+        }
+
+    }
 }
