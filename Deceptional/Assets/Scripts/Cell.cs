434aec839ffced09cf6208132032b504a51c2b42
Oscar Losada <oscarlosu@gmail.com>
2016-03-23 15:06:07 +0100

    Started grid tests.

---
+++ Deceptional/Assets/Scripts/Cell.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cell : MonoBehaviour {
+    public List<Cell> Adjacent;
+    public bool Free;
+
+    public void On
+}
---
+++ Deceptional/Assets/Scripts/Grid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+    public List<Cell> Cells;
+    public List<Cell> FreeCells;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
