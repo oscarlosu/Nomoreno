f709e31070cc16d75e693f6ae2ef78922a96fc82
Alexander Kirk Jørgensen <alkii900@msn.com>
2016-03-24 10:18:18 +0100

    Backup Commit

---
+++ Deceptional/Assets/Scripts/TextWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class TextWrapper : MonoBehaviour {
+        public TextMesh Text;
+        public int MaxLineCharacters;
+
+        public void BreakLine() {
+            StringBuilder sb = new StringBuilder(Text.text);
+
+            if (Text.text.Length > MaxLineCharacters) {
+                for (int i = 0; i < sb.Length; i++) {
+                    if (i % MaxLineCharacters == 0) {
+                        sb.Insert(i, "\n");
+                    }
+                }
+            }
+        }
+    }
+}
